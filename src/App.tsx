ed33683176e2f1a23f8ea5721cde15455bf91fdc
Stephan-Neo <stepan.kazancev.04@bk.ru>
2023-09-28 14:41:28 +0600

    feat: create response on backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState(null);
+
+  const callBackendAPI = async () => {
+    const response = await fetch('http://localhost:8000/getUsers');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+
+  useEffect(() => {
+    callBackendAPI()
+    .then(res => {
+      setState(res.text)
+      console.log(res.text)
+    })
+    .catch(err => console.log(err));
+  }, [])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          GTSK - company future
+          <div>
+            Users:<span></span>
+            {state}
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
