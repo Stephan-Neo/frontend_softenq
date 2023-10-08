e05e904c11b6b999fcdd224838f8571f7f6e0775
Stephan-Neo <stepan.kazancev.04@bk.ru>
2023-10-09 00:09:37 +0600

    feat: add proxy on vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import environments from './src/config/environments';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: environments.baseApiUrl,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
